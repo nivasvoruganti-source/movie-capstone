fc56c813460f08e89451a97131e9d59ede8055f0
nivasvoruganti-source <nivasvoruganti@gmail.com>
2026-01-08 11:43:13 +0530

    Make backend ready for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -32,3 +32,8 @@ def home():
     return " Movie API Running"
 
 app.run(host="0.0.0.0", port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
